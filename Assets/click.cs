3685c696730ec1b6e4a383b5d917470ebbd00091
buckngaw <aon_natjaya@hotmail.com>
2017-08-14 22:35:17 +0700

    click

---
+++ Assets/click.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click : MonoBehaviour {
+
+    public int myValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        if (Input.GetKey("mouse 0"))
+        {
+            myValue++;
+            print("Box Clicked!");
+        }
+    }
+}
